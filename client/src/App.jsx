1633f2278beee59e5ba8e1158c1fbcd80b900ae1
kanishka-sahoo <kanishka.sa.hoo@gmail.com>
2024-02-07 08:56:59 +0530

    Added main page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ServerListItem from './pages/ServerListItem'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col min-h-screen bg-neutral-950 flex-1">
+      <div className="rounded-md bg-neutral-700 mx-2 mt-2 p-1 flex flex-row">
+        <h1 className='text-white font-semibold text-3xl'>NetMusic</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="flex-row mx-2 mt-2 flex-1 flex">
+        <div className='rounded-md w-1/2 md:w-1/3 lg:w-1/4 2xl:w-1/6 bg-neutral-700 p-2 flex flex-col'>
+          <h2 className="text-white text-xl font-semibold">Servers</h2>
+          <ServerListItem />
+          <ServerListItem />
+          <ServerListItem />
+          <ServerListItem />
+
+        </div>
+        <div className='ml-2 rounded-md flex-1 bg-neutral-700 p-2'>
+          <h2 className="text-white text-xl font-semibold">Server</h2>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="mx-2 mt-2 flex flex-row flex-1 max-h-16"></div>
+    </div>
   )
 }
 
